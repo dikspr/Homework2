b4c6887c7c5bcd0101b5743767882b6c1ff2aa5c
dikspr <dikspr@gmail.com>
2019-07-24 17:18:03 +0300

    Закончил ДЗ6

--- DZ6/main.py
+++ DZ6/main.py
@@ -17,21 +17,32 @@
 # 7. Протестируйте работу метода show_part класса Sentence. Исправьте ошибки, чтобы метод работал.
 # Подсказка: ранее part был свойством класса Word, а теперь это метод классов Noun и Verb.
 
-
+# перенес класс и пятого Д/з
 class Word:
     def __init__(self, text):
         self.text = text
 
+# Создал два новых класса
 class Noun(Word):
     __grammar = 'сущ'
+
+    def part(self):
+        if self.__grammar == 'сущ':
+            return 'существительное'
+
 
 class Verb(Word):
     __grammar = 'гл'
 
+    def part(self):
+        if self.__grammar == 'гл':
+            return 'глагол'
+
+# перенес класс и пятого Д/з и немного поправил метод show_parts
+# (добавил скобки после words[i].part, т.к. это теперь метод)
 class Sentence:
     def __init__(self, content):
         self.content = content
-
 
     def show(self):
         sent = ''
@@ -39,17 +50,20 @@ class Sentence:
             sent = sent + words[i].text + ' '
         return sent
 
-
     def show_parts(self):
-        parts = [words[i].part for i in self.content]
+        parts = [words[i].part() for i in self.content]
         parts = set(parts)
         parts = list(parts)
         return parts
+
 
 words = []
 words.append(Noun("собака"))
 words.append(Verb("ела"))
 words.append(Noun("колбасу"))
 words.append(Noun("кот"))
+
 prime = Sentence([1, 3])
 print(prime.show())
+print(prime.show_parts())
+
