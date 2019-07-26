8aff585f6e318c5823137115257432173e95d05d
dikspr <dikspr@gmail.com>
2019-07-26 15:24:18 +0300

    Выполнил третий и второй пункты ДЗ7. Исправил концовку и счетчик хомяков.

--- DZ7/Game.py
+++ DZ7/Game.py
@@ -7,6 +7,8 @@ hamsters_count = 4
 class Game:
     map = '****\n****\n****\n****'
     gameon = True
+    happy_message = 'WOW!!!! You won!'
+    hamsters_count_now = hamsters_count
 
     def __init__(self):
         self.player = Player()
@@ -28,6 +30,7 @@ class Game:
         for h in self.hamsters:
             if h.health > 0:
                 s = self.add_point(h.position, str(h.id), s)
+
         print(s)
 
 
@@ -78,15 +81,31 @@ class Game:
                 self.move_player(self.directions[direction])
             else:
                 print(self.hamsters[int(hamster) - 1].id, 'was killed')
+                self.hamsters_count_now -= 1
 
     def start(self):
         game.render_map()
         while self.gameon:
+            # print(self.hamsters_count_now)
+            if self.hamsters_count_now == 0:
+                print(self.happy_message)
+                self.gameon = False
             command = input('Insert command: ')
             if command in ['a', 's', 'd', 'w']:
                 self.move_player(command)
                 self.render_map()
+            if command == 'e':
+                self.player.wait()
+            if command == 'q':
+                self.gameon = False
 
 
 game = Game()
 game.start()
+
+# 2. Нормальную обработку границ карты мы сделали, когда делали метод перемещения по карте. Ошибки нет.
+# 3. Для исправления ошибки с концовкой в классе Game ввел свойство hamsters_count_now,
+# которое показывает текущее количество хомяков.
+# В методе on_move, когда хомяк умирает, это свойство уменьшается на 1.
+# В методе start в начале цикла проверяем не количество хомяков в списке hamsters, которое у нас в игре не меняется,
+# а свойство hamsters_count_now.
